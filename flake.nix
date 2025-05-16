4d0063faf52146c174147e71b105bb7044f81bd8
Brad Hesson <brad.hesson@outlook.com>
2025-05-16 00:47:16 -0600

    updated flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,35 @@
 {
+  description = "Build a cargo project";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs = flakes: flakes.flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import flakes.nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
+      pkgs = import flakes.nixpkgs { inherit system; };
+      craneLib = flakes.crane.mkLib pkgs;
+      commonArgs = {
+        src = craneLib.cleanCargoSource ./.;
+        strictDeps = true;
+        buildInputs = [ pkgs.openssl.dev ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
       };
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      crate = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
     in
     {
-      devShell = with pkgs; mkShell {
-        packages = [
-          rustup
-        ];
-        buildInputs = [
-          pkg-config
-          openssl.dev
+      packages.default = crate;
+      apps.default = flakes.flake-utils.lib.mkApp { drv = crate; };
+      devShells.default = craneLib.devShell {
+        checks = { inherit crate; };
+        packages = with pkgs; [
+          nixpkgs-fmt
+          nixd
         ];
       };
     }
   );
 }
-

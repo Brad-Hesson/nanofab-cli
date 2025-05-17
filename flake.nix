a3bf44caf2fe1611276374819ae395601e7a0871
Brad Hesson <brad.hesson@outlook.com>
2025-05-16 20:02:24 -0600

    finished making flake build

--- flake.nix
+++ flake.nix
@@ -4,27 +4,46 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = flakes: flakes.flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import flakes.nixpkgs { inherit system; };
-      craneLib = flakes.crane.mkLib pkgs;
-      commonArgs = {
-        src = craneLib.cleanCargoSource ./.;
+      fenix = flakes.fenix.packages.${system};
+      crane = (flakes.crane.mkLib pkgs).overrideToolchain (fenix.combine [
+        fenix.stable.defaultToolchain
+        fenix.stable.rust-src
+      ]);
+      crateArgs = {
+        src = crane.cleanCargoSource ./.;
         strictDeps = true;
         buildInputs = [ pkgs.openssl.dev ];
         nativeBuildInputs = [ pkgs.pkg-config ];
       };
-      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-      crate = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+      cargoArtifacts = crane.buildDepsOnly crateArgs;
+      crate = crane.buildPackage (crateArgs // { inherit cargoArtifacts; });
     in
     {
+      checks = {
+        inherit crate;
+        clippy = crane.cargoClippy (crateArgs // {
+          inherit cargoArtifacts;
+          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+        });
+        documentation = crane.cargoDoc (crateArgs // {
+          inherit cargoArtifacts;
+        });
+        formatting = crane.cargoFmt { inherit (crateArgs) src; };
+      };
       packages.default = crate;
       apps.default = flakes.flake-utils.lib.mkApp { drv = crate; };
-      devShells.default = craneLib.devShell {
-        checks = { inherit crate; };
+      devShells.default = crane.devShell {
+        inputsFrom = [ crate ];
         packages = with pkgs; [
           nixpkgs-fmt
           nixd
